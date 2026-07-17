db5debcee4a81031955173371235d27368445528
Hammyal chaudry <hammayalchaudry@gmail.com>
2026-07-18 00:39:31 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Dishes from './components/Dishes';
-import About from './components/About';
-import Footer from './components/Footer';
+
 
 function App() {
   const [cartCount, setCartCount] = useState(0);
